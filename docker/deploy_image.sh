f319bb1084b94e9fb929e68478fd17d18d4395e1
root <root@sherif-asus-tuf-gaming-a15.tail47968b.ts.net>
2025-10-06 20:45:45 +0000

    Update Docker setup. Update README

--- docker/deploy_image.sh
+++ docker/deploy_image.sh
@@ -12,11 +12,5 @@ docker run -it --net=host --privileged --gpus all \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$(pwd):/catkin_ws/src/obj_manipulation" \
-    --workdir /catkin_ws \
-    nvidia_melodic /entrypoint.sh
-
-# docker run --rm -it --gpus all \
-#   --device /dev/nvidia-caps:/dev/nvidia-caps \
-#   -e NVIDIA_VISIBLE_DEVICES=all \
-#   -e NVIDIA_DRIVER_CAPABILITIES=all \
-#   nvidia/cuda:11.8.0-base-ubuntu22.04 nvidia-smi
+    robot \
+    bash
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 # Source the ROS environment
-source /opt/ros/melodic/setup.bash
+source /opt/ros/noetic/setup.bash
 
 # Compile the source code
 catkin_make
-# source /stonefish_ws/devel/setup.bash
-
-# Start the ROS master
-# roscore &
+source /catkin_ws/devel/setup.bash
 
 # Launch terminal to execute commands
 exec bash
-
-# Use to force the use of the dedicated GPU
-# __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia
