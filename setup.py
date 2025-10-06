8b63407e0a67eb86ba8c60ead0846395c9276196
Sherif-Sameh <sherif252525@gmail.com>
2025-10-06 09:38:55 +0000

    Add setup.py to pkg. Update CMakeLists.txt and package.xml accordingly.

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+package_name = 'obj_manipulation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(include=[package_name, f'{package_name}.*']),
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='',
+    maintainer_email='you@example.com',
+    description='Object manipulation ROS1 package',
+    license='MIT',
+)
