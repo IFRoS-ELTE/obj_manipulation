3ac3fdc00bc2b314fbafebb579908f2e0ca1b8c3
Muhammad Faran Akram <faran1218@gmail.com>
2025-12-01 15:50:45 +0100

    add gripper control in move_node, add home state in moveit

--- scripts/move_node.py
+++ scripts/move_node.py
@@ -8,7 +8,7 @@ from geometry_msgs.msg import PoseStamped, Point
 from std_msgs.msg import Bool
 from visualization_msgs.msg import Marker, MarkerArray
 from tf.transformations import quaternion_from_euler, quaternion_matrix
-
+from dh_gripper_msgs.msg import GripperCtrl
 
 class MoveNode(object):
     def __init__(self):
@@ -22,12 +22,14 @@ class MoveNode(object):
 
         # Publishers + Subscribers
         self.goal_pub = rospy.Publisher("/goal_pose", PoseStamped, queue_size=1)
-         
+        # Publisher to low-level gripper controller (expects dh_gripper_msgs/GripperCtrl)
+        self.grip_ctrl_pub = rospy.Publisher("/gripper/ctrl", GripperCtrl, queue_size=1)
         rospy.Subscriber("/grasp_estimation_node/grasp_pose",
                          PoseStamped, self.plan_to_pose, queue_size=1)
         rospy.Subscriber("/move_node/allow_execution",
                          Bool, self.allow_execution_cb, queue_size=1)
-
+        rospy.Subscriber("/gripper_ctrl", Bool, self.ctrl_gripper, queue_size=1)
+        
         # MoveIt config
         print("Planning frame:", self.arm_group.get_planning_frame())
         print("End effector link:", self.arm_group.get_end_effector_link())
@@ -38,13 +40,14 @@ class MoveNode(object):
         self.arm_group.allow_replanning(True)
         self.arm_group.set_goal_position_tolerance(0.01)
         self.arm_group.set_goal_orientation_tolerance(0.05)
-        self.arm_group.set_max_velocity_scaling_factor(0.05)
-        self.arm_group.set_max_acceleration_scaling_factor(0.05)
+        self.arm_group.set_max_velocity_scaling_factor(0.5)
+        self.arm_group.set_max_acceleration_scaling_factor(0.5)
 
         # Threading 
         self._lock = threading.Lock()
         self._latest_plan = None
         self.latest_pose = None
+        self.use_gripper = False
         rospy.loginfo("Move node ready, waiting for grasp poses and execution trigger.")
 
     # --------------------------- CALLBACKS ---------------------------
@@ -93,14 +96,20 @@ class MoveNode(object):
         try:
             rospy.loginfo("Executing trajectory...")
             self.arm_group.execute(self._latest_plan, wait=True)
+            if self.use_gripper:
+                self.ctrl_gripper(Bool(data=False))
+                rospy.sleep(3)
+                self.move_home()
+            self.use_gripper = False
             rospy.loginfo("Execution complete.")
             self._latest_plan = None
         finally:
             # self.arm_group.stop()
             # self.arm_group.clear_pose_targets()
             self.move_offset()
+            self.use_gripper = True
             self._lock.release()
-
+    
     def move_offset(self):
         offset = 0.19
         mat = quaternion_matrix([self.latest_pose.pose.orientation.x,
@@ -113,7 +122,7 @@ class MoveNode(object):
         self.latest_pose.pose.position.z += mat[2, 0] * offset
 
         self._plan_with_rrtconnect(self.latest_pose) 
-        
+        # self.use_gripper = True
         self.arm_group.stop()
         self.arm_group.clear_pose_targets()
 
@@ -130,7 +139,29 @@ class MoveNode(object):
         else:
             rospy.logwarn("Primary planner failed")
 
- 
+    def ctrl_gripper(self, msg):
+        ''' rostopic pub /gripper/ctrl dh_gripper_msgs/GripperCtrl initialize: false
+        position: 1000.0
+        force: 50.0
+        speed: 0.0''' 
+        grip_msg = GripperCtrl()
+        grip_msg.initialize = False
+        if msg.data:
+            grip_msg.position = 1000.0
+            grip_msg.force = 25.0
+            grip_msg.speed = 0.0
+        else:
+            grip_msg.position = 0.0
+            grip_msg.force = 25.0
+            grip_msg.speed = 0.0
+        self.grip_ctrl_pub.publish(grip_msg)
+
+    def move_home(self):
+        self.arm_group.set_named_target("home")
+        self.arm_group.go(wait=True)
+        rospy.sleep(3)
+        self.ctrl_gripper(Bool(data=True))
+
 if __name__ == "__main__":
     node = MoveNode()
     rospy.spin()
